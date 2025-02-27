773274cb90b57f6a13a591ebfc6fb42f04d4d56d
Joseph Nader <josephnaderwj@gmail.com>
2025-02-27 14:50:04 +0200

    *****UPDATE*****

---
+++ Unit_3_Embedded_C/Assignment_Lesson_3/app.c
@@ -0,0 +1,9 @@
+#include "uart.h"
+
+unsigned char string_buffer [100] = " Learn In Depth : << Joseph Nader Sophy >> ";
+unsigned char const string_buffer_2[100] = "to create ro data section";
+
+void main(void)
+{
+uart_send_string(string_buffer);
+}
